07ba6de869f1f60a785b5878713b124c9f99f330
g000001 <chiba.masaomi@gmail.com>
2010-06-26 22:12:26 +0900

    普段使っている環境をパッケージにしてみる試み

---
+++ g000001.asd
@@ -0,0 +1,5 @@
+(asdf:defsystem :G000001
+  :name "g000001"
+  :description "g000001"
+  :version "1"
+  :components ((:FILE "package")))
---
+++ g000001.lisp
@@ -0,0 +1,9 @@
+(IN-PACKAGE :G000001)
+
+(DEFUN GITHUB-INSTALL (USER-NAME NAME)
+  (ASDF-INSTALL:INSTALL
+   (FORMAT NIL
+           "http://github.com/~A/~A/tarball/master"
+           USER-NAME
+           NAME)))
+
