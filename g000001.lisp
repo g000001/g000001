be50bfdc1cb50e87b2952b27e5a2f16a7e7c6ebe
g000001 <chiba.masaomi@gmail.com>
2011-06-20 18:35:15 +0900

    * pkg-bind追加

--- g000001.lisp
+++ g000001.lisp
@@ -363,7 +363,7 @@ which conveniently return a form to undo what they did.
      (lambda (x)
        (and (symbolp x)
             (not (symbol-package x))))
-     (kl:flatten tree)))
+     (sl::flatten-safe tree)))
 
   (defun count-symbol-names (syms)
     (let ((tab (make-hash-table :test 'equal)))
@@ -521,7 +521,7 @@ which conveniently return a form to undo what they did.
     ;; clパッケージから:testを受け付けるオペレーターを探す
     (do-symbols (sym :cl)
       (when (and (fboundp sym)
-                 (member 'test (member '&key (kl:flatten (swank::arglist sym)))
+                 (member 'test (member '&key (sl:multiple-value-do   (swank::arglist sym)))
                          :key #'princ-to-string :test #'string-equal))
         (push sym it)))))
 
@@ -677,5 +677,60 @@ which conveniently return a form to undo what they did.
           (push s not-imported)
           (import s package)))
     not-imported))
+
+;; pkg-bind
+
+(defclass intern-form ()
+  ((name :initarg :name)
+   (package :initarg :package)))
+
+
+(defmethod print-object ((obj intern-form) stream)
+  (format stream
+          "#.(CL:INTERN ~S ~S)"
+          (slot-value obj 'name)
+          (slot-value obj 'package)))
+
+;(make-instance 'intern-form :name "FOO" :package "CL")
+;=> #.(CL:INTERN "FOO" "CL")
+
+(defun up-symbol (elt pkg)
+  (typecase elt
+    (symbol
+       (let ((name (string elt)))
+         (make-instance 'intern-form
+                  :name name
+               :package (package-name
+                         (let ((elt-pkg (symbol-package elt)))
+                           (cond ((eq elt-pkg (find-package pkg))
+                                  pkg)
+                                 ;;
+                                 ((and (eq elt-pkg (find-package *package*))
+                                       (not (find-symbol pkg)))
+                                  pkg)
+                                 ;;
+                                 ('T elt-pkg)))))))
+    ;;
+    (otherwise elt)))
+
+(defun symbol-to-intern-form (tree pkg)
+  (cond ((null tree)
+         tree)
+        ;;
+        ((atom (car tree))
+         (let ((elt (car tree)))
+           (cons (if (eq 'pkg-bind elt)
+                     'pkg-bind
+                     (up-symbol elt pkg))
+                 (symbol-to-intern-form (cdr tree) pkg))))
+        ;;
+        ('T (cons (symbol-to-intern-form (car tree) pkg)
+                  (symbol-to-intern-form (cdr tree) pkg)))))
+
+(defmacro pkg-bind (pkg &body body)
+  `(progn
+     ,@(read-from-string
+        (write-to-string
+         (symbol-to-intern-form body (package-name pkg))))))
 
 ;; eof
