dba30272b584ccb081f68381dbbd7e22b51e4a3b
g000001 <chiba.masaomi@gmail.com>
2020-02-26 02:13:41 +0900

    clean up

--- g000001.asd
+++ g000001.asd
@@ -1,36 +1,82 @@
 (asdf:defsystem :g000001
   :name "g000001"
   :description "g000001"
-  :version "5"
+  :version "8"
   :serial t
-  :components ((:file "package")
+  :components ((:file "shell/package")
+               #||
                (:file "readtable")
+               (:file "decl-utils")
+               (:file "decls")
                (:file "package.g000001")
                (:file "function.g000001")
                (:file "g000001")
                (:file "lispm")
-               #-lispworks (:file "ja")
-               (:file "tools")
-               (:file "pprint")
+               (:file "tools.tao")
+               (:file "tools.cl")
+               ;; (:file "pprint")
+               (:file "tools.arc")
+               (:file "twitter.arc")
+               (:file "tools.scm")
+               ||#
                )
-  :depends-on (:root
-               :named-readtables
-               :yaclml
-               :cl-html-parse
-               :tao-compat
-               :swank
-               :series
-               :series-ext
-               :kmrcl
-               :fare-utils
-               :alexandria
-               :shibuya.lisp
-               :cl-who
+  :depends-on
+  ()
+  #|(:root
+   :star
+   :g000001.xpath
+   ;; :cl-chatwork
+   :g000001.tools
+   :fmt
+   :reader.r6rs
+   :cl-unicode
+   :root.package.it
+   :root.package.scheme-keyword
+   :SRFI-87
+   :SRFI-19
+   :SRFI-46
+   :cl-json
+   :lambda.time
+   :named-readtables
+   :yaclml
+   :cl-html-parse
+   :tao-compat
+   :gauche-compat
+   :babel
+   :swank
+   :series
+   :xpath
+   :cxml-stp
+   :series-ext
+   :kmrcl
+   :macroexpand-dammit
+   :fare-utils
+   :alexandria
+   :shibuya.lisp
+   :cl-who
+   :drakma
+   :closure-html
                ;:cl-twitter
-               :xyzzy-compat
-               :zl-compat
-               :snow-match
-               #-ecl :srfi-13
+   :xyzzy-compat
+   :zl-compat
+   :snow-match
+   :arc-compat
+   :gauche-compat.text.tr
+   #-ecl :srfi-13
+   :srfi-48
+   :srfi-26
 ;               :executor
 ;               :sclf
-               ))
+   :colorize
+   :cl-twitter
+   :objectlisp
+   :cl-delicious
+   #-(:or lispworks ccl) :cool
+   :g000001.npr-utils
+   #-(:or lispworks ccl) :g000001.comic-natalie-utils
+   :g000001.utils.booklog
+   :g000001.html
+   :g000001.twitter
+   ;; :cl-markdown
+   :toot
+   )|#)
---
+++ shell/package.lisp
@@ -0,0 +1,137 @@
+(in-package :g1)
+
+#|||
+           Put             Provide 
+           IN              IN-package 
+           Seven           Shadow 
+           EXtremely       EXport 
+           Random          Require 
+           USEr            USE-package 
+           Interface       Import 
+           COmmands        COntents of package/module
+|||#
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (let ((pkg "https://github.com/g000001/shell/package"))
+    ;; Put
+    (provide "https://github.com/g000001/shell/package")
+    (when (find-package pkg)
+      (delete-package pkg))
+    ;; In
+    (setq *package*
+          (make-package pkg
+                        :nicknames '(#:shell.pkg)
+                        :use '(#:cl)))))
+(progn
+  ;; Seven
+  (shadow '())
+  ;; Extremely
+  (export '(*package-path*
+            pkg-bind
+            use-package-soft
+            show-packages))
+  ;; Random
+  '(require)
+  ;; User
+  (use-package "CL")
+  ;; Interface
+  (import '())
+  ;; Commands
+  )
+
+;;; --------------------------------------------------------------------------
+;;; use-package-soft
+;;; --------------------------------------------------------------------------
+(defun use-package-soft (package-to-use
+                         &optional (package #+sbcl (sb-int:sane-package)
+                                            #-sbcl *package*))
+  (let ((not-imported () ))
+    (do-external-symbols (s package-to-use)
+      (if (find-symbol (string s))
+          (push s not-imported)
+          (import s package)))
+    not-imported))
+
+
+;;; ---------------------------------------------------------------------------
+;;; package path
+;;; ---------------------------------------------------------------------------
+(defvar *PACKAGE-PATH* '() )
+
+(defun auto-import (name)
+  (let (ans)
+    (dolist (pkg (reverse *package-path*))
+      (when (and (find-package pkg)
+                 (find-symbol (string name) pkg))
+        (let ((sym (intern (string name) pkg)))
+          (shadowing-import sym)
+          (push pkg ans))))
+    ans))
+
+
+;;; -------------------------------------------------------------------------
+;;; pkg-bind
+;;; -------------------------------------------------------------------------
+(defclass intern-form ()
+  ((name :initarg :name)
+   (package :initarg :package)))
+
+
+(defmethod print-object ((obj intern-form) stream)
+  (format stream
+          "#.(CL:INTERN ~S ~S)"
+          (slot-value obj 'name)
+          (slot-value obj 'package)))
+
+;(make-instance 'intern-form :name "FOO" :package "CL")
+;=> #.(CL:INTERN "FOO" "CL")
+
+(defun up-symbol (elt pkg)
+  (typecase elt
+    (symbol
+       (let ((name (string elt)))
+         (make-instance 'intern-form
+                  :name name
+               :package (package-name
+                         (let ((elt-pkg (symbol-package elt)))
+                           (cond ((eq elt-pkg (find-package pkg))
+                                  ;; current (pkg-bind :foo foo::x) => (progn foo::x)
+                                  pkg)
+                                 ;;
+                                 ((and (eq elt-pkg (find-package *package*))
+                                       (find-symbol (string elt) pkg))
+                                  ;; current (pkg-bind :foo x) => (progn foo::x) or (progn current::x)
+                                  pkg)
+                                 ;; current (pkg-bind :foo bar::x) => (progn bar::x)
+                                 ('T elt-pkg)))))))
+    ;;
+    (otherwise elt)))
+
+(defun symbol-to-intern-form (tree pkg)
+  (cond ((null tree)
+         tree)
+        ;;
+        ((atom (car tree))
+         (let ((elt (car tree)))
+           (cons (if (eq 'pkg-bind elt)
+                     'pkg-bind
+                     (up-symbol elt pkg))
+                 (symbol-to-intern-form (cdr tree) pkg))))
+        ;;
+        ('T (cons (symbol-to-intern-form (car tree) pkg)
+                  (symbol-to-intern-form (cdr tree) pkg)))))
+
+(defmacro pkg-bind (pkg &body body)
+  `(progn
+     ,@(read-from-string
+        (write-to-string
+         (symbol-to-intern-form body (package-name pkg))))))
+
+
+(defun package-exports (pkg &aux (pkg (find-package pkg)))
+  (let ((ans '() ))
+    (do-external-symbols (s pkg ans)
+      (push s ans))))
+
+
+;;; eof
